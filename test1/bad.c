60afe18064588fbdab156ceac866c9491c1ae464
Sean Li <sean.jason.li@gmail.com>
2020-10-03 22:41:43 -0700

    Adding test files

---
+++ test1/bad.c
@@ -0,0 +1,5 @@
+/* $Id: bad.c,v 1.1 2020-03-04 20:22:07-08 - - $ */
+
+This is a bad file.
+cc won't compile it.
+
---
+++ test3/hello.c
@@ -0,0 +1,8 @@
+/* $Id: hello.c,v 1.1 2020-02-03 15:28:10-08 - - $ */
+
+#include "hello.h"
+
+void hello (void) {
+   printf ("Hello, world.\n");
+}
+
